7728adcb1e271b2f7247092003d23cfc85c208e2
Callum <clpullinger@gmail.com>
2018-10-05 03:10:07 +0100

    push heroku

--- commercial/admin.py
+++ commercial/admin.py
@@ -3,5 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from portfolio.models import Project
+from .models import Booking
 
-# Register your models here.
+# Register your models here
+
+admin.site.register(Booking)
